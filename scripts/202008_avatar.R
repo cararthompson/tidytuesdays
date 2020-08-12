6f194991d43b9c4a31dfab01f7868f3e8e1a40dd
Cara Thompson <cara.r.thompson@gmail.com>
2020-08-12 21:32:41 +0100

    Basic graphs complete

--- scripts/202008_avatar.R
+++ scripts/202008_avatar.R
@@ -3,6 +3,9 @@ library(tidyverse)
 library(tvthemes)
 library(tidytuesdayR)
 library(ggforce)
+library(extrafont)
+loadfonts()
+library(ggforce)
 
 
 # Get the data ####
@@ -18,21 +21,37 @@ avatar <- tt_data$avatar %>%
             chapter_num = unique(chapter_num), 
             imdb = unique(imdb_rating))
 
+theme_darkAvatar <- theme_avatar %>%
+  
+  
+  # Plot it! ####
 
-# Plot it! ####
+sweetspot <- ggplot(avatar, 
+                    aes(x = total_chat, y = imdb)) +
+  geom_smooth(colour = "white", alpha = .6) +
+  geom_point(aes(colour = book), alpha = .8, size = 3) +
+  theme_avatar(title.font = "Slayer",
+               text.font = "Slayer",
+               title.size = 14) +
+  scale_colour_manual(name = "",
+                      # using avatar_pal with 4 colours
+                      values = c("#015E05", "#FF4500", "#1DB4D3"),
+                      labels = c("Earth", "Fire", "Water"),
+                      guide = F) +
+  labs(title = "The anti sweet spot of chattiness",
+       subtitle = "Across all three books, IMDB ratings drop 
+when the characters say between 2000 and 2200 words.") 
 
-sweetspot <- ggplot(avatar, aes(x = chapter_num, y = total_chat, fill = imdb)) +
-  geom_bar(stat = "identity") +
-#  theme("avatar") + 
-  labs(title = "Avatar: the last airbender - The anti sweet spot of chattiness") +
-  facet_grid(book~.)
 
-
-fire <- ggplot(avatar, aes(x = chapter_num, y = total_chat, colour = imdb)) +
-  geom_bar(stat = "identity") +
-  theme("avatar") + 
-  labs(title = "Avatar: the last airbender - The anti-sweetspot of chattiness") +
-  facet_grid(book~.)
+fire <- ggplot(filter(avatar, book == "Fire"), aes(x = chapter_num, y = total_chat, fill = imdb)) +
+  geom_bar(stat = "identity") + # make bars thinner
+  theme_avatar(title.font = "Slayer",
+               text.font = "Slayer",
+               title.size = 14) +
+  ylim(c(0, 2750)) +
+  coord_polar(theta = "y", start = 4.71, clip = "on") +
+  # add x axis in right place
+  scale_fill_avatar(palette = "FireNation", nrow(avatar), type = "continuous") 
 
 
 # Export to create making-of gif 
