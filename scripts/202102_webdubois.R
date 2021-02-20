93206d27396261d7c42babab7a255b06f5d1232f
Cara Thompson <cara.r.thompson@gmail.com>
2021-02-20 11:40:50 +0000

    Added gganimate output

--- scripts/202102_webdubois.R
+++ scripts/202102_webdubois.R
@@ -1,7 +1,8 @@
-# Art!
+# DuBois Challenge
 library(tidyverse)
 library(tidytuesdayR)
 library(extrafont)
+library(gganimate)
 
 ## Get the data ----
 tt_data <- tt_load(2021, week = 8)
@@ -56,8 +57,9 @@ for(i in 1:length(years)){
   xs[[paste0("xs_", years[i])]] <- r[[i]]*-sin(theta)
   colour[[paste0("colour_", years[i])]] <- c(rep("no", length(theta) - ceiling(hvds[[i]]/100)), 
                                              rep(colour_values[[i]], ceiling(hvds[[i]]/100)))
-  text_val[[paste0("text_val_", years[i])]] <- c(round(seq(0, hvds[[i]], length.out = sum(colour[[i]] != "no"))),
-                                                 rep(hvds[[i]], sum(colour[[i]] == "no")))
+  # Create text_val in reverse to this
+  text_val[[paste0("text_val_", years[i])]] <- c(rep(hvds[[i]], sum(colour[[i]] == "no")),
+                                                 round(seq(hvds[[i]], 0, length.out = sum(colour[[i]] != "no"))))
 }
 
 spiral_data <- as_tibble(c(r, xs, ys, colour, text_val, 
@@ -78,7 +80,10 @@ static_text_data <- tibble("year" = c(paste(years[1:4], "---- `` "),
                     "x_value" = rep(-4, 6),
                     "hjust_value" = rep(1, 6),
                     "static_text" = c(format(furniture$`Houshold Value (Dollars)`, big.mark = ",")))
-                                      
+# 
+# spiral_data <- spiral_data %>%
+#   left_join(static_text_data[c("year", "y")])
+#                                       
 
 ## Plot it! ----
 p <- ggplot(data = spiral_data) +
@@ -96,19 +101,57 @@ for(i in length(years):1) {
                shape = 18, size = 3.48) 
 }
 
-p + 
+static_spiral <- p + 
+  geom_text(data = static_text_data,
+            aes(x = -4, y = y, label = static_text),
+            hjust = 1, size = 2.5, family = "Highway Gothic Wide") +
   geom_segment(data = NULL, aes(x = -1, xend = -1, y = 100, yend = 160), 
               size = 3, colour = colour_values[7]) +
   geom_text(data = static_text_data,
             aes(x = x_year, y = y, label = year),
                 hjust = 0, size = 2.5, family = "Highway Gothic Wide") +
-  geom_text(data = static_text_data,
-            aes(x = x_value, y = y, label = static_text),
-            hjust = 1, size = 2.5, family = "Highway Gothic Wide") +
   labs(title = "\nASSESSED VALUE OF HOUSEHOLD AND KITCHEN FURNITURE\nOWNED BY GEORGIA NEGROES.",
        caption = "\n\nGraphic: @cararthompson | #TidyTuesday #DuBoisChallenge\nData: Anthony Starks, Allen Hillery and Sekou Tyler")
 
 
-## Export final plot ----
-ggsave(filename = file.path("../plots", "202102_webdubois.png"), 
-       dpi = 600, width = 6.15, height = 7.7)
+## Export static plot ----
+ggsave(plot = static_spiral, filename = file.path("../plots", "202102_webdubois.png"), 
+       dpi = 600, width = 6.15, height = 7.69)
+
+
+anim_spiral <- p + 
+  # this is layer 7
+  geom_text(aes(x = -4, y = max(ys_1899), label = text_val_1899),
+            hjust = 1, size = 2.5, family = "Highway Gothic Wide") +
+  geom_text(aes(x = -4, y = max(ys_1895), label = text_val_1895),
+            hjust = 1, size = 2.5, family = "Highway Gothic Wide") +
+  geom_text(aes(x = -4, y = max(ys_1890), label = text_val_1890),
+            hjust = 1, size = 2.5, family = "Highway Gothic Wide") +
+  geom_text(aes(x = -4, y = max(ys_1885), label = text_val_1885),
+            hjust = 1, size = 2.5, family = "Highway Gothic Wide") +
+  geom_text(aes(x = -4, y = max(ys_1880), label = text_val_1880),
+            hjust = 1, size = 2.5, family = "Highway Gothic Wide") +
+  geom_text(aes(x = -4, y = max(ys_1875), label = text_val_1875),
+            hjust = 1, size = 2.5, family = "Highway Gothic Wide") +
+  geom_segment(data = NULL, aes(x = -1, xend = -1, y = 100, yend = 160), 
+               size = 3, colour = colour_values[7]) +
+  geom_text(data = static_text_data,
+            aes(x = x_year, y = y, label = year),
+            hjust = 0, size = 2.5, family = "Highway Gothic Wide") +
+  labs(title = "\nASSESSED VALUE OF HOUSEHOLD AND KITCHEN FURNITURE\nOWNED BY GEORGIA NEGROES.",
+       caption = "\n\nGraphic: @cararthompson | #TidyTuesday #DuBoisChallenge\nData: Anthony Starks, Allen Hillery and Sekou Tyler") +
+  transition_components(increment) +
+  enter_fade() +
+  # Exclude the layers of text
+  shadow_mark(past = T, exclude_layer = c(7:12)) 
+
+
+## Export gif ----
+anim_save(animate(plot = anim_spiral,  nframes = 2000), 
+          filename = file.path("../plots", "202102_webdubois.gif"),
+          res = 600, width = 6.15, height = 7.69, units = "in",
+          end_pause = 300, rewind = F, fps = 100)
+# I the cropped and modified the speed of the gif with an online tool
+# because I couldn't get the frame rate needed through gganimate
+
+
